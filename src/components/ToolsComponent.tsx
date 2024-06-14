06711bf4d1575088a5c4722833105b745d614b1e
Aurelio Ribeiro <aurelio74123@gmail.com>
2024-06-14 19:14:49 -0300

    feat: create ToolsComponent

---
+++ src/components/ToolsComponent.tsx
@@ -0,0 +1,11 @@
+type Props = {
+    label:string;
+}
+
+export const ToolsComponent = ({label}:Props)=>{
+    return(
+        <div  className=" shadow-md shadow-black bg-[#151414] rounded-lg text-sm flex justify-center items-center border-[1px] border-[#505252] hover:animate-bounce hover:shadow-xl  hover:shadow-black py-[4px] px-3 ">
+            {label}
+        </div>
+    )
+}
