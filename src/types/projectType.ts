cf11187421446732031fc3bfd6da3e05abca4edf
Aurelio Ribeiro <aurelio74123@gmail.com>
2024-06-18 12:04:00 -0300

    style: change color shadow

---
+++ src/types/projectType.ts
@@ -0,0 +1,13 @@
+import { StaticImageData } from "next/image";
+
+export type projectType = { 
+    imageProject?: StaticImageData;
+    id: number;
+    title: string;
+    description: string;
+    linkGithub: string;
+    urlProject?: string;
+
+    usedtechnologies :string[];
+    
+}
