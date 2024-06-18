5f93e41db8597be1d14fcaa41ee8a95f3d2f6f83
Aurelio Ribeiro <aurelio74123@gmail.com>
2024-06-18 10:43:45 -0300

    feat: create section Projects

---
+++ src/assets/data/projectsData-en.ts
@@ -0,0 +1,64 @@
+import { projectType } from "@/types/projectType";
+import imgColeman from "@/assets/images/RonieColeman.png";
+import imgPomodoro from "@/assets/images/pomodoro.png";
+import imgSibi from "@/assets/images/sibi.png";
+import imgStarbuks from "@/assets/images/starbuks.png";
+import imgTodoReact from "@/assets/images/todoreact.png";
+import imgQuizReact from "@/assets/images/QuizReact.png";
+
+export const projectData_en: projectType[] = [
+    {
+        id: 1,
+        imageProject: imgColeman,
+        title: "Tribute to Ronnie Coleman",
+        description: "This is a tribute page project for Ronnie Coleman, created to test my front-end skills. The page aims to honor and highlight the exceptional career of renowned bodybuilder Ronnie Coleman. The page is fully responsive, adapting to different screen sizes and devices.",
+        linkGithub: "https://github.com/aureliodeboa/Tribute-Ronnie-Coleman",
+        usedtechnologies: ["Html", "Css", "Git"],
+        urlProject: "https://aureliodeboa.github.io/Tribute-Ronnie-Coleman/"
+    },
+    {
+        id: 2,
+        imageProject: imgPomodoro,
+        title: "Pomodoro Timer",
+        description: "The Pomodoro Timer is a customizable web application that allows users to personalize their own Pomodoro timer. With this application, you can set work time, break time, choose colors, and even select your preferred font. This application is a useful tool for increasing productivity and improving time management.",
+        linkGithub: "https://github.com/aureliodeboa/Pomodoro",
+        usedtechnologies: ["Html", "Css", "Javascript", "Git"],
+        urlProject: "https://silver-chebakia-3fd8cb.netlify.app/"
+    },
+    {
+        id: 3,
+        imageProject: imgSibi,
+        title: "SIBI in the Palm of Your Hand",
+        description: "SIBI (Integrated Libraries System of UNIVASF) is an application developed using the Flutter framework. It serves as a user guide for the library system of the Federal University of Vale do São Francisco (UNIVASF), offering quick and intuitive access to SIBI information.",
+        linkGithub: "https://github.com/aureliodeboa/AplicativoSIBI?tab=readme-ov-file",
+        usedtechnologies: ["Flutter", "Dart", "Scrum", "Git"],
+        urlProject: "https://www.youtube.com/watch?v=p7WKsB0vFxk&feature=youtu.be"
+    },
+    {
+        id: 4,
+        imageProject: imgQuizReact,
+        title: "Quiz App",
+        description: "This project is a Quiz application developed with React, Next.js, Tailwind CSS, and TypeScript. It allows users to answer a series of questions and displays correct and incorrect alternatives at the end.",
+        linkGithub: "https://github.com/aureliodeboa/Quiz-app?tab=readme-ov-file",
+        usedtechnologies: ["Typescript", "Tailwind", "React", "Next.js", "Html", "Git"],
+        urlProject: "https://fastidious-kangaroo-7e1acc.netlify.app/"
+    },
+    {
+        id: 5,
+        imageProject: imgTodoReact,
+        title: "To-Do List",
+        description: "This is a simple To-Do List project developed using React, Next.js, and TypeScript. The application allows users to add, mark as completed, and remove tasks from the list.",
+        linkGithub: "https://github.com/aureliodeboa/To-do-with-React?tab=readme-ov-file",
+        usedtechnologies: ["Typescript", "Tailwind", "React", "Next.js", "Html", "Git"],
+        urlProject: "https://master--gentle-snickerdoodle-46b5f2.netlify.app/"
+    },
+    {
+        id: 6,
+        imageProject: imgStarbuks,
+        title: "Starbucks",
+        description: "This is a Starbucks homepage project developed to practice CSS grid-template property skills. In this project, I used only HTML and CSS to create a simple replica of the Starbucks homepage.",
+        linkGithub: "https://github.com/aureliodeboa/Starbucks?tab=readme-ov-file",
+        usedtechnologies: ["Html", "Css", "Git"],
+        urlProject: "https://inquisitive-cendol-2fd1cd.netlify.app/"
+    }
+];
--- src/components/ToolsComponent.tsx
+++ src/components/ToolsComponent.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export const ToolsComponent = ({label}:Props)=>{
     return(
-        <div  className=" shadow-md shadow-black dark:bg-[#151414] rounded-lg text-sm flex justify-center items-center border-[1px] border-[#505252] hover:animate-bounce hover:shadow-xl  hover:shadow-black py-[4px] px-3 ">
+        <div  className=" shadow-md shadow-black dark:bg-[#151414] rounded-lg text-xs sm:text-sm flex justify-center items-center border-[1px] border-[#505252] hover:animate-bounce hover:shadow-xl  hover:shadow-black py-[4px] px-3 ">
             {label}
         </div>
     )
