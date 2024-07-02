067a6357246ad693f985ee6f0bf9837a0c91b673
Aurelio Ribeiro <aurelio74123@gmail.com>
2024-07-01 22:16:07 -0300

    style bg

--- src/components/ButtonMidia.tsx
+++ src/components/ButtonMidia.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const ButtonMidia = ({children, clickAction}:Props)=>{
     return(
-        <button onClick={clickAction} className=" rounded-md m-[2px] p-2 bg-[#ffffff] border border-black  text-black dark:bg-[#1E1E1E] dark:text-white flex flex-row justify-center">
+        <button onClick={clickAction} className=" rounded-md m-[2px] p-2 bg-[#ffffff] border border-black dark:border-[#1E1E1E] text-black dark:bg-[#1E1E1E] dark:text-white flex flex-row justify-center">
             {children}
         </button>
     )
