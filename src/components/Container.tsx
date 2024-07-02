77f6dd608b3abcab6c957bc62830b87406610a66
Aurelio Ribeiro <aurelio74123@gmail.com>
2024-07-01 22:28:46 -0300

    fix mobile

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from "@/contexts/ThemeContext";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 
 type Props = {
     children: ReactNode;
@@ -7,12 +7,12 @@ type Props = {
 
 export const Container = ({ children }: Props) => {
     const themeCtx = useTheme();
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
     return (
         <div className="w-full h-dvh overflow-y-scroll flex flex-col justify-around bg-[#FFFFFF] text-black dark:bg-[#09090B] dark:text-white relative">
-            {/* Background Circles for Dark Mode */}
-            <div className="hidden dark:block absolute top-1/4 left-1/3 w-72 h-72 bg-yellow-500 rounded-full blur-3xl opacity-30"></div>
-            <div className="hidden dark:block absolute top-1/2 right-1/4 w-64 h-64 bg-green-600 rounded-full blur-2xl opacity-30"></div>
-            <div className="hidden dark:block absolute bottom-1/4 left-1/4 w-80 h-80 bg-yellow-700 rounded-full blur-2xl opacity-30"></div>
+        
             {children}
         </div>
     );
