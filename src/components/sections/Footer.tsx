3a1dfa2a62d82cd8b075c32d81c88f3629b4f20b
aureliodeboa <aurelio74123@gmail.com>
2024-07-05 15:37:17 -0300

    feat add footer page

---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,22 @@
+import { useTranslation } from "react-i18next";
+
+export const Footer = () => {
+    const currentYear = new Date().getFullYear();
+    const { t } = useTranslation();
+    return (
+        <div className="py-4 mt-8">
+            <hr className="border-gray-700 mb-4" />
+            <div className="text-center">
+                <p>
+                {t("footer.rights")} | 
+                    <a href="https://github.com/seu-usuario" target="_blank" rel="noopener noreferrer" className="mx-1 dark:text-white text-black font-bold hover:underline">
+                        Aurelio Ribeiro 
+                    </a>
+                    | Copyright  &copy; {currentYear} 
+                   
+                </p>
+                <p></p>
+            </div>
+        </div>
+    );
+};
