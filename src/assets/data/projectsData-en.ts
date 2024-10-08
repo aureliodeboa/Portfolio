27bf85815950650580cbc739e2fac46f8012eca6
aureliodeboa <aurelio74123@gmail.com>
2024-10-08 11:51:38 -0300

    feat: some updates and fix name page starbucks

--- src/assets/data/projectsData-en.ts
+++ src/assets/data/projectsData-en.ts
@@ -5,21 +5,30 @@ import imgSibi from "@/assets/images/sibi.png";
 import imgStarbuks from "@/assets/images/starbuks.png";
 import imgTodoReact from "@/assets/images/todoreact.png";
 import imgQuizReact from "@/assets/images/QuizReact.png";
-import imgI9site from "@/assets/images/sitei9.png"
+import imgI9site from "@/assets/images/sitei9.png";
+import micael from "@/assets/images/micael_portfolio.png";
 
 export const projectData_en: projectType[] = [
     {
         id: 1,
+        imageProject: micael,
+        title: "Micael Contador",
+        description: "A professional website developed for Micael Henrique, an accountant specialized in tax management and consultancy for entrepreneurs and freelancers. The website connects clients to the services offered and provides information about his professional activities.",
+        linkGithub: "https://github.com/aureliodeboa/micael-portfolio",
+        usedtechnologies: ["TypeScript", "Tailwind", "React", "Next.js", "Git", "Kanban", "Framer Motion"],
+        urlProject: "https://micaelcontador.netlify.app/"
+    },
+    {
+        id: 2,
         imageProject: imgI9site,
         title: "I9 Junior Website",
         description: "I9 is a junior enterprise at the Federal University of the São Francisco Valley, linked to the Computing Department. It provides technological solutions for companies and entrepreneurs by developing websites, applications, and customized systems. I developed the front-end of this website.",
         linkGithub: "https://github.com/inove-jr/i9-website",
         usedtechnologies: ["TypeScript", "Tailwind", "React", "Next.js", "Git", "Kanban", "Scrum"],
         urlProject: "https://inove-jr.vercel.app/"
-        
     },
     {
-        id: 2,
+        id: 3,
         imageProject: imgPomodoro,
         title: "Pomodoro Timer",
         description: "The Pomodoro Timer is a customizable web application that allows users to personalize their own Pomodoro timer. With this application, you can set work time, break time, choose colors, and even select your preferred font. This application is a useful tool for increasing productivity and improving time management.",
@@ -28,7 +37,7 @@ export const projectData_en: projectType[] = [
         urlProject: "https://silver-chebakia-3fd8cb.netlify.app/"
     },
     {
-        id: 3,
+        id: 4,
         imageProject: imgStarbuks,
         title: "Starbucks",
         description: "This is a Starbucks homepage project developed to practice CSS grid-template property skills. In this project, I used only HTML and CSS to create a simple replica of the Starbucks homepage.",
@@ -37,7 +46,7 @@ export const projectData_en: projectType[] = [
         urlProject: "https://inquisitive-cendol-2fd1cd.netlify.app/"
     },
     {
-        id: 4,
+        id: 5,
         imageProject: imgTodoReact,
         title: "To-Do List",
         description: "This is a simple To-Do List project developed using React, Next.js, and TypeScript. The application allows users to add, mark as completed, and remove tasks from the list.",
@@ -46,7 +55,7 @@ export const projectData_en: projectType[] = [
         urlProject: "https://master--gentle-snickerdoodle-46b5f2.netlify.app/"
     },
     {
-        id: 5,
+        id: 6,
         imageProject: imgQuizReact,
         title: "Quiz App",
         description: "This project is a Quiz application developed with React, Next.js, Tailwind CSS, and TypeScript. It allows users to answer a series of questions and displays correct and incorrect alternatives at the end.",
@@ -55,7 +64,7 @@ export const projectData_en: projectType[] = [
         urlProject: "https://fastidious-kangaroo-7e1acc.netlify.app/"
     },    
     {
-        id: 6,
+        id: 7,
         imageProject: imgSibi,
         title: "SIBI in the Palm of Your Hand",
         description: "SIBI (Integrated Libraries System of UNIVASF) is an application developed using the Flutter framework. It serves as a user guide for the library system of the Federal University of Vale do São Francisco (UNIVASF), offering quick and intuitive access to SIBI information.",
@@ -64,15 +73,12 @@ export const projectData_en: projectType[] = [
         urlProject: "https://www.youtube.com/watch?v=p7WKsB0vFxk&feature=youtu.be"
     },
     {
-        id: 7,
+        id: 8,
         imageProject: imgColeman,
         title: "Tribute to Ronnie Coleman",
         description: "This is a tribute page project for Ronnie Coleman, created to test my front-end skills. The page aims to honor and highlight the exceptional career of renowned bodybuilder Ronnie Coleman. The page is fully responsive, adapting to different screen sizes and devices.",
         linkGithub: "https://github.com/aureliodeboa/Tribute-Ronnie-Coleman",
         usedtechnologies: ["Html", "Css", "Git"],
         urlProject: "https://aureliodeboa.github.io/Tribute-Ronnie-Coleman/"
-        
-    },
-    
-   
+    }
 ];
